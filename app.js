d5237eaba594edeb62a76776f72af9ae43a3350b
xrr2016 <13286904669@163.com>
2017-01-26 00:18:46 +0800

    :fire: edit

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(ctx => {
-  ctx.body = "Hello Koa2!"
+app.use(function *(next){
+  let start = Date.now()
+  yield next
+
+  const ms = Date.now - start
+  this.set('X-Response-Time',ms + 'ms')
+
+})
+app.use(function *(next){
+  let start = Date.now()
+  yield next
+  const ms = Date.now() - start
+  console.log('%s %s - %s',this.method,this.url,ms)
+})
+
+
+// app.use(ctx => {
+//   ctx.body = "Hello Koa2!"
+// })
+app.use(function *(){
+  this.body = "Hello World!"
 })
 
 app.listen(2333)
